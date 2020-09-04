c5df07a2ec1b70209857b201467ac0ecf8cfdff5
Jackson Matias <jsmatias@thoughtworks.com>
2020-09-04 09:35:08 -0300

    Initial Project

---
+++ training/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  training
+//
+//  Created by Jackson Matias on 04/09/20.
+//  Copyright © 2020 jackson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Fernanda, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
